f161bb0ecb56c642771c98b8387534b9f610c3fa
kyoro-chan <61698254+kyoro-chan@users.noreply.github.com>
2020-03-17 15:34:12 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+$(function(){
+
+  $(".start").click(function(){
+    $(".arab").css("display", "none");
+    $(".samba").show();
+    $(".btn-push").text('retry').css('padding-left','5px');
+    $(".result").show();
+  });
+  
+});
